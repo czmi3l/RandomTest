6bbcbbbc77cca951d8071dccf670bd99e02c18fe
czmi3l <czmiel1224@gmail.com>
2014-12-06 22:56:15 +0100

    1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        //Tworzy plik DebugRandom.png w folderze aplikacji w bin
+        static void Main(string[] args)
+        {
+            Bitmap bmp = new Bitmap(600, 600);
+            Random rnd = new Random();
+            string path = Environment.CurrentDirectory + "Random.png";
+            for (int i = 0; i < 600; i++)
+            {
+                for (int j = 0; j < 600; j++)
+                {
+                    if(rnd.Next(0,2) == 0)
+                        bmp.SetPixel(i, j, Color.Black);
+
+                }
+            }
+            bmp.Save(path);
+        }
+    }
+}
